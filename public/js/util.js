22b4528e9e19bb1f03dbf5725ed8903e0cfbb875
BeatriceFish <1175765719@qq.com>
2021-01-26 11:04:19 +0800

    进度条

--- public/js/util.js
+++ public/js/util.js
@@ -1,4 +1,26 @@
+// 这里定义一些工具函数，为了其他地方复用
+
+
 (function(window,undefined){
+
+    function startNProgress(){
+        NProgress.configure({
+            showSpinner: false
+        });
+        NProgress.set(0.4);// 默认设置40% NProgress.set(0) 等价与 NProgress.start()
+        let interval = setInterval(function(){
+            NProgress.inc(); // 以小量递增
+        },200)
+
+        $(window).on('load',()=>{
+            clearInterval(interval)
+            NProgress.done()
+        })
+    }
+
+    startNProgress();
+
+    
 
     let util = {
         date_format:function(date,format="YYYY-MM-DD HH:mm:ss"){
